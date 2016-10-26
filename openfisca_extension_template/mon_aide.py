25fd36c6a65739143c498f242054a12029c3df26
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2016-10-27 01:28:28 +0200

    Use Openfisca-core#4 syntax

--- openfisca_extension_template/mon_aide.py
+++ openfisca_extension_template/mon_aide.py
@@ -7,13 +7,13 @@ from openfisca_france.model.base import *  # noqa analysis:ignore
 
 class mon_aide(Variable):
     column = FloatCol
-    entity_class = Familles
+    entity = Familles
     label = u"Prestation locale qui consite à donner 100€ par mois et par enfants."
 
-    def function(self, simulation, period):
+    def function(famille, period, legislation):
         period = period.this_month
-        nombre_enfants = simulation.calculate('af_nbenf', period)
-        montant_par_enfant = simulation.legislation_at(period.start).ma_collectivite.mon_aide.montant
+        nombre_enfants = famille('af_nbenf', period)
+        montant_par_enfant = legislation(period).ma_collectivite.mon_aide.montant
 
         return period, nombre_enfants * montant_par_enfant
 
