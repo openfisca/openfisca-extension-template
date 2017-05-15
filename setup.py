3c61f967a1297b8771f93521a1c0809fa1ba17eb
Florian Pagnoux <florian.pagnoux@data.gouv.fr>
2017-05-16 01:22:49 +0200

    Rename pip package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Openfisca-MA_COLLECTIVITE",
+    name="OpenFisca-Extension-Template",
     version="1.0.0",
     description="Plugin OpenFisca pour les aides sociales de ma collectivité",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
