99ac3d3ccb884e5c2ea5212fa1befd28405ce3b5
Michel Blancard <michel.blancard@data.gouv.fr>
2017-08-02 15:26:21 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OpenFisca-Extension-Template",
-    version="1.1.0",
+    version="1.1.1",
     description="An OpenFisca extension that adds some variables to an already-existing tax and benefit system",
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
     author="",
