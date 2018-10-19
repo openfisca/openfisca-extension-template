3d638a7fdc9985551c4ce9eaa208e3d6c87308f7
Mauko Quiroga <mauko.quiroga@data.gouv.fr>
2018-10-19 19:03:10 +0200

    Test against the packaged version

--- setup.py
+++ setup.py
@@ -4,21 +4,33 @@ from setuptools import setup, find_packages
 setup(
     name="OpenFisca-Extension-Template",
     version="1.1.6",
+    author="OpenFisca Team",
+    author_email = 'contact@openfisca.org',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        ],
     description="An OpenFisca extension that adds some variables to an already-existing tax and benefit system",
+    keywords = 'benefit microsimulation social tax',
     license="http://www.fsf.org/licensing/licenses/agpl-3.0.html",
-    author="",
-    packages=find_packages(),
-    include_package_data=True,
+    url = "https://github.com/openfisca/extension-template",
+    include_package_data = True,  # Will read MANIFEST.in
+    data_files = [
+        ("share/openfisca/openfisca-extension-template", ["CHANGELOG.md", "LICENSE", "README.md"]),
+        ],
     install_requires = [
-        'OpenFisca-Country-Template >= 3,  < 4',
+        "OpenFisca-Country-Template >= 3,  < 4",
         ],
     extras_require = {
-        'test': [
-            'nose',
+        "dev": [
+            "autopep8 == 1.4.0",
+            "flake8 >= 3.5.0, < 3.6.0",
+            "flake8-print",
+            "pycodestyle >= 2.3.0, < 2.4.0",  # To avoid incompatibility with flake
             ]
         },
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-    ]
-)
+    packages=find_packages(),
+    )
